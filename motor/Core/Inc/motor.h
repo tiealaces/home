1f44c1a6fec6016ca8524b23d6b8aa45d759f1e6
tiealaces <seojh1250@gmail.com>
2024-08-01 17:22:54 +0900

    update

---
+++ motor/Core/Inc/motor.h
@@ -0,0 +1,23 @@
+/*
+ * motor.h
+ *
+ *  Created on: Aug 1, 2024
+ *      Author: USER
+ */
+
+#ifndef INC_MOTOR_H_
+#define INC_MOTOR_H_
+
+#include "main.h"
+
+void Forward();
+
+void Backward();
+
+void Right();
+
+void Left();
+
+void STOP();
+
+#endif /* INC_MOTOR_H_ */
